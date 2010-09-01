6e27a4c04943c3b26cb759526a043d0e5c7d7b9e
Chris Salch <emeraldd.chris@gmail.com>
2010-08-31 22:48:23 -0500

    Setting a basic execution environment for the parser.

---
+++ src/markup.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+
+# Petup a sane execution execution environment
+use strict;
+use warnings;
+use diagnostics;
+
+# Provide a readable display of our parsed data structure
+use Data::Dumper;
+
+# Mask any libraries in the current directory
+no lib '.';
+
+# Make sure that we are actually dealing with uft8
+binmode STDIN, ':encoding(utf8)';
+binmode STDOUT, ':encoding(utf8)';
+
+
+=pod
+=head1 USAGE
+
+=cut
+
+# Do we have a file on the command line or should we be 
+# looking for a stream?
+my $source= @ARGV ? &slurp($ARGV[0]) : &slurp(\*STDIN);
+
+print $source;
+
+=head1 UTILITY FUNCTIONS
+
+
+=cut
+
+
+=head2 slurp
+
+Takes in a file handle and slurps its entire contents into a scalar.
+
+=cut
+
+sub slurp {
+    my ($file)=@_;
+
+    local $/; # set the end of line marker to undef 
+
+    # If we were passed a file handle, slurp it and return
+    # otherwise, we treat our argument as a filename.
+    if(ref $file eq 'GLOB') {
+	return <$file>;
+    }
+
+
+    open my $fh, '<', $file
+	or die "Unable to open file $file due to: $!";
+    
+    binmode $fh, ":encoding(utf8)"; # set utf8 encoding
+    
+    my $content=<$fh>;
+
+    close $fh; # clean up properly
+
+    return $content;
+}
+
+
+
+
+
