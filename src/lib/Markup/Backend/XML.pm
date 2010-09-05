ea17000816643f53bf462387c902cf5b88760219
Chris Salch <emeraldd.chris@gmail.com>
2010-09-05 00:47:15 -0500

    Well, this breaks all kinds of things but is probably the way I need to go :(

--- src/lib/Markup/Backend/XML.pm
+++ src/lib/Markup/Backend/XML.pm
@@ -4,6 +4,9 @@ use strict;
 
 use base 'Markup::Base';
 
+# list of tags that are considered containers and need to have
+# a $/ after the opening tag
+my %container = map { $_ => 1 } qw/body blockquote ol ul/;
 
 =head1 NAME
 
@@ -14,7 +17,6 @@ Markup::Backend::XML - Produces simple XML output from a Markup::Tree instance
 
 =cut
 
-
 =head1 METHODS
 
 =head2 string
@@ -24,14 +26,22 @@ Convert the passed in Markup::Tree structure into a xml string.
 =cut
 
 sub string {
-    my ($self, $tree)=@_;
+    my ($self, $tree, $extra_indent)=@_;
     
     my $name=$tree->name;
     my $text=$tree->text;
     my $string.='';
 
+    # make sure that $extra_indent is a number
+    $extra_indent=defined($extra_indent)?$extra_indent:0;
+    
+    # if we do not have a container tag, indent
+    unless($container{$name}) {
+	$extra_indent+=1;
+    }
+
     # construct an indent block 
-    my $indent= ' ' x (4 * int($tree->indent /2));
+    my $indent= ' ' x (4 * (int($tree->indent /2) + $extra_indent));
 
     if($tree->verbatim) {
 	$string=$indent . ($text?"<$name>":"<$name/>");
@@ -40,10 +50,11 @@ sub string {
 
     } else {
 	# if we have no internals, start with an empty body
-	if($tree->inline) {
-	    $string=$indent . ((@{$tree->body})?"<$name>":"<$name/>");
-	} else {
-	    $string=$indent . ((@{$tree->body})?"<$name>$/":"<$name/>");
+	$string=$indent . ((@{$tree->body})?"<$name>":"<$name/>");
+	
+	# put a $/ after opening container tags
+	if($container{$name}) {
+	    $string.=$/;
 	}
 
 	#TODO: Actually do something with the backend
@@ -63,7 +74,7 @@ sub string {
 		}
 
 	    } elsif(ref $_) { # a complex tag
-		$string.=$self->string($_);
+		$string.=$self->string($_, $extra_indent);
 
 	    } else { # we have a tag with inline content
 		$string.=$_;
@@ -71,9 +82,12 @@ sub string {
 	    }
 	    
 	}
-
+	
+	unless($extra_indent) {
+	    $string.=$indent;
+	}
 	# did we have an empty body tag?
-	$string.=$indent . ((@{$tree->body})?"</$name>":'');
+	$string.=(@{$tree->body})?"</$name>":'';
     }
 
     # # convert indentations to 4 spaces
