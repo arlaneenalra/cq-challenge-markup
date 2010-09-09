0b5ae70231778c49a23d73c839a416a13f61e533
Chris Salch <emeraldd.chris@gmail.com>
2010-09-08 22:01:00 -0500

    Add a few nice features to the markup.pl driver

--- src/markup.pl
+++ src/markup.pl
@@ -27,7 +27,7 @@ xml document.
 
 =head1 SYNOPSIS
 
-markup.pl [filename]
+markup.pl [--no-links] [filename] [-o outputfile]
 
 If no filename is provided, markup.pl will expect to receive input via stdin 
 and send output to stdout.
@@ -40,23 +40,85 @@ cat test.txt | markup.pl
 
 =cut
 
-my $tokenizer=Markup::Tokenizer->new();
+# parse arguments into reasonable values
+my ($filename, $links, $output)=&parse_args(@ARGV);
+
+my $tokenizer=Markup::Tokenizer->new(links => $links);
+
+
 my $parser=Markup::Parser->new(tokenizer => $tokenizer);
 my $backend=Markup::Backend::XML->new();
 
 # Do we have a file on the command line or should we be 
 # looking for a stream?
-my $source=@ARGV ? &slurp($ARGV[0]) : &slurp(\*STDIN);
+my $source=$filename ? &slurp($filename) : &slurp(\*STDIN);
 
 # parse the source
 my $tree=$parser->parse($source);
 
-use Data::Dumper;
-print &Dumper($tree);
+my $string=$backend->string($tree);
 
-print $backend->string($tree);
+# write to a given file or STDOUT
+if($output) {
+    open OUTPUT, '>', $output
+	or die "Unable to open output file $@";
+    
+    binmode OUTPUT, ':encoding(utf8)';
+    
+    print OUTPUT $string;
+
+    close OUTPUT;
+    
+} else {
+    # use STDOUT
+    print $string;
+}
 
 =head1 INTERNALS
 
+=head2 parse_args
+
+Convert the arguments array into a set of scalars we can use for 
+configuration choices.
+
 =cut
+sub parse_args {
+    my @args=@_;
+
+    my ($filename, $links, $output)=('', 1, '');
+   
+    
+    # if we have any arguments, parse them
+    if(@args) {
+	
+	# did they ask for no links?
+	( $links )=grep { $args[$_] eq '--no-links' } 0..$#args;
+	
+	# clear the --no-links from the array
+	if(defined($links)) {
+	    $args[$links]='';
+	    $links=0;
+	}
+	
+	
+	# look for -o 
+	my ( $o_index )=grep { $args[$_] eq '-o' } 0..$#args;
+
+	# did we match?
+	if(defined($o_index)) {
+	    $output=$args[$o_index+1];
+	    
+	    # clear -o filename from the array
+	    $args[$o_index]='';
+	    $args[$o_index+1]='';
+	}
+
+	# treat the first true value as an input file name
+	($filename)=grep { $_ } @args;
+
+    }
+    
+    return ($filename, $links, $output);
+}
+
 
