940b71d9433513b5359d7bbc5addc62ae29e0f36
Chris Salch <emeraldd.chris@gmail.com>
2010-09-10 22:52:44 -0500

    Added code to clear out backup files to count

--- count.sh
+++ count.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-#echo "Cleaning . . ."
+echo "Cleaning . . ."
 #make clean
-#echo
+find . -path ./.git -prune -o -name '*~' -exec rm {} \;
+echo
 
 echo "Source Statistics:"
 wc `find . -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\|pl\|pm\)' | grep -v 'test\/test_eyeball\|CMake\|build/\|\.git'`
