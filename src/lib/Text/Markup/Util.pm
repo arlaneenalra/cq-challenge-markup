d29df66771e2a50e4209cfa15f5b818fe2dab151
Chris Salch <emeraldd.chris@gmail.com>
2010-09-11 21:02:27 -0500

    Adjusting markup.pl and Text::Markup::Util to handle calling various rendering backends

--- src/lib/Text/Markup/Util.pm
+++ src/lib/Text/Markup/Util.pm
@@ -1,8 +1,11 @@
 package Text::Markup::Util;
 
 use strict;
+use warnings;
 
 use base 'Exporter';
+
+use Carp;
 
 =head1 NAME
 
@@ -15,7 +18,7 @@ This is a catch all module for functions that are used in several locations.
 =cut
 
 # setup a list of functions that may be exported into using modules
-our @EXPORT_OK= qw/slurp/;
+our @EXPORT_OK= qw/slurp parse_args/;
 
 
 =head1 FUNCTIONS 
@@ -29,7 +32,7 @@ Takes in a file handle and slurps its entire contents into a scalar.
 sub slurp {
     my ($file)=@_;
 
-    local $/; # set the end of line marker to undef 
+    local $/=undef; # set the end of line marker to undef 
 
     # If we were passed a file handle, slurp it and return
     # otherwise, we treat our argument as a filename.
@@ -38,7 +41,7 @@ sub slurp {
     }
 
     open my $fh, '<', $file
-        or die "Unable to open file $file due to: $!";
+        or croak "Unable to open file $file due to: $!";
     
     binmode $fh, ":encoding(utf8)"; # set utf8 encoding
     
@@ -50,5 +53,93 @@ sub slurp {
     return $content;
 }
 
+=head2 parse_args
+
+Convert the arguments array into a set of scalars we can use for 
+configuration choices.  Unmatched, defined arguments are returned.
+Parameters may be defined to accept one or no arguments.  If a given
+parameter accepts one argument, its value is treated as true or false.
+If a given parameter is defined as accepting one argument, the value 
+immediately following that parameter in the argument array is assumed 
+to be the argument.  Matched parameters have no defined order.
+
+Example:
+
+=over
+
+
+=back
+
+=cut
+sub parse_args {
+    my ($args_ref, $param_ref)=@_;
+
+    if(!@{$args_ref}) {
+        return;
+    }
+
+    # walk the list of possible parameters and see
+    # if any of them match
+    foreach my $param ( keys %{$param_ref}) {
+        my ($accepts, $var_ref)=@{$param_ref->{$param}}{'accepts', 'var'};
+
+        my $value=&parse_param($param, $accepts, $args_ref);
+
+        # only assign to our variable reference if
+        # we actually found something.
+        if(defined($value)) {
+            ${$var_ref}=$value;
+        }
+
+    }
+    
+    # prune all undefined values from the argument list
+    my @unmatched=grep { defined($_) } @{$args_ref};
+    
+    return @unmatched;
+}
+
+=head2 parse_param
+
+Look for an argument that accepts a single parameter, return that 
+parameter and remove the parameter, flag from the argument array
+
+=cut
+
+sub parse_param {
+    my ($param, $accepts, $args_ref)=@_;
+
+    print "PARAM:$param:$accepts:$/";
+
+    # look for the argument
+    my ( $index )=grep {
+        $args_ref->[$_] and
+            $args_ref->[$_] eq $param 
+    } 0..$#{$args_ref};
+
+    # if this parameter does not accept an argument, we 
+    # just need to know if it was in the list
+    if(!$accepts) {
+
+        # remove the parameter from our param list
+        if(defined($index)) {
+            $args_ref->[$index]=undef;
+
+            return 1;
+        }
+        
+    } elsif(defined($index)) {
+        my $value=$args_ref->[$index+1];
+        
+        # clear -o filename from the array
+        $args_ref->[$index]=undef;
+        $args_ref->[$index+1]=undef;
+
+        return $value;
+    }
+    
+    # no match found
+    return;
+}
 
 1;
